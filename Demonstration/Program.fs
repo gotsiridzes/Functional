c4af7a496b92e719d27f4cf4b07b4c1c29c603d0
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2022-11-06 00:47:25 +0400

    Initialize F# Demo project

---
+++ Demonstration/Program.fs
@@ -0,0 +1,14 @@
+﻿type Currency = Currency of string;; // currency object contains string
+let usd = Currency("usd");;
+type Amount = Amount of decimal * Currency;;
+
+let deposit = Amount(5m, usd);;
+
+type Money = 
+    | Cash of decimal * Currency
+    | Gift of decimal * Currency * System.DateTime;;
+
+let instrument = Cash(20m, usd);
+
+
+type Adder = Money * Amount * System.DateTime -> Money * Amount;;
